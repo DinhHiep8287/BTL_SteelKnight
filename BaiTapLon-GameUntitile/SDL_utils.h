1449439ebf1a3f6dbe6a5e0b17c16070f3d197a4
DinhHiep8287 <124423632+DinhHiep8287@users.noreply.github.com>
2023-02-11 05:00:13 +0700

    Update Header

---
+++ BaiTapLon-GameUntitile/SDL_utils.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <SDL.h>
+#include <SDL_mixer.h>
+#include <SDL_ttf.h>
+#include <SDL_image.h>
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+void logSDLError(std::ostream& os,
+    const std::string& msg, bool fatal = false);
+
+void initSDL(SDL_Window*& window, SDL_Renderer*& renderer);
+
+void quitSDL(SDL_Window* window, SDL_Renderer* renderer);
+
+void waitUntilKeyPressed();
